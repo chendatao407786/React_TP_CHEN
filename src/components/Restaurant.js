cf06be045c93a145af37b18bec9d23e48617ff5c
CHEN Datao <chendatao1126@gmail.com>
2018-10-25 22:57:22 +0200

    Initial commit

---
+++ src/components/Restaurant.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+
+function Restaurant(props) {
+
+  return (
+    <tr>
+      <td>{props.restaurant.name}</td>
+      <td>{props.restaurant.cuisine}</td>
+      <td><button onClick={() => props.removeRestaurants(props.restaurant)}>delete</button></td>
+    </tr>
+    // <li onClick={() => props.removeRestaurants(props.name)}>{props.name}</li>
+  );
+}
+
+export default Restaurant;
